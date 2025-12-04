7d0962651b67e27ffc2d55ecd7ae0e61bbc66fa8
github-action-benchmark <github@users.noreply.github.com>
2025-12-04 14:18:30 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for baacb65c34bb74505545900fb6d7c5af897112e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1764857698575,
+  "lastUpdate": 1764857910850,
   "repoUrl": "https://github.com/RodrigoVillar/sample-reexecution-custom-benchmark",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReexecuteRange/[1,50000]-Config-default-Runner-dev",
             "value": 3.3,
+            "unit": "block_verify_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[1,50000]-Config-default-Runner-dev",
+            "value": 4,
+            "unit": "mgas/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "committer": {
+            "email": "rodrigo.villar@avalabs.org",
+            "name": "Rodrigo Villar",
+            "username": "RodrigoVillar"
+          },
+          "distinct": true,
+          "id": "baacb65c34bb74505545900fb6d7c5af897112e3",
+          "message": "chore: nit",
+          "timestamp": "2025-12-04T09:18:07-05:00",
+          "tree_id": "d3ed66e8b9f37d3e675a78394c43d667bf69cb49",
+          "url": "https://github.com/RodrigoVillar/sample-reexecution-custom-benchmark/commit/baacb65c34bb74505545900fb6d7c5af897112e3"
+        },
+        "date": 1764857910605,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "BenchmarkReexecuteRange/[1,50000]-Config-default-Runner-dev",
+            "value": 1.1,
+            "unit": "block_accept_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[1,50000]-Config-default-Runner-dev",
+            "value": 2.2,
+            "unit": "block_parse_ms/ggas"
+          },
+          {
+            "name": "BenchmarkReexecuteRange/[1,50000]-Config-default-Runner-dev",
+            "value": 3.4,
             "unit": "block_verify_ms/ggas"
           },
           {
